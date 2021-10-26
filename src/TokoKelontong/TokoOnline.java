e5711fc9023b84d56da456a485beadd2387c4e2a
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2021-10-26 09:30:27 +0700

    added main class in toko_online

--- src/TokoKelontong/TokoOnline.java
+++ src/TokoKelontong/TokoOnline.java
@@ -2,6 +2,20 @@ package TokoKelontong;
 
 public class TokoOnline {
     public static void main(String[] args) {
-        System.out.println("Hello from TokoOnline");
+        Member orang = new Member();
+        Karyawan pegawai = new Karyawan();
+        Barang barang = new Barang();
+        Transaksi transaksi = new Transaksi();
+
+        Laporan laporan = new Laporan();
+        laporan.Laporan(orang);
+        laporan.Laporan(barang);
+        laporan.Laporan(transaksi, barang);
+
+        System.out.println();
+        transaksi.prosesTransaksi(orang, transaksi, barang);
+        laporan.Laporan(orang);
+        laporan.Laporan(barang);
+        laporan.Laporan(transaksi, barang);
     }
 }
