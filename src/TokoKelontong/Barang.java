42925d72b7dda058e3d6cfb894fd485fb0763565
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2021-10-26 08:47:35 +0700

    added new class in toko_online

--- src/TokoKelontong/Barang.java
+++ src/TokoKelontong/Barang.java
@@ -20,4 +20,39 @@ public class Barang {
         this.stok.add(20);
         this.harga.add(15000);
     }
+
+    //get jumlah
+    public int getJmlhBarang(){
+        return this.namaBarang.size();
+    }
+
+    //anything correlated with namaBarang
+    public void setNamaBarang(String _namaBarang){
+        this.namaBarang.add(_namaBarang);
+    }
+    public String getNamaBarang(int idBarang){
+        return this.namaBarang.get(idBarang);
+    }
+    //anything correlated with namaBarang
+
+    //anything correlated with stok
+    public void setStok(int _stok){
+        this.stok.add(_stok);
+    }
+    public int getStok(int idBarang){
+        return this.stok.get(idBarang);
+    }
+    public void editStok(int idBarang, int stok){
+        this.stok.set(idBarang, stok);
+    }
+    //anything correlated with stok
+
+    //anything correlated with harga
+    public void setHarga(int harga){
+        this.harga.add(harga);
+    }
+    public int getHarga(int idBarang){
+        return this.harga.get(idBarang);
+    }
+    //anything correlated with harga
 }
--- src/TokoKelontong/User.java
+++ src/TokoKelontong/User.java
@@ -2,9 +2,9 @@ package TokoKelontong;
 
 public interface User {
     void setNama(String nama);
-    void setAlamat(String nama);
-    void setTelepon(String nama);
-    String getNama();
-    String getAlamat();
-    String getTelepon();
+    void setAlamat(String almaat);
+    void setTelepon(String telp);
+    String getNama(int id);
+    String getAlamat(int id);
+    String getTelepon(int id);
 }
