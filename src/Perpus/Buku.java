1a4e34aaff7f0ef308911b3ce232f18f377f473a
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2021-11-02 19:55:10 +0700

    tambahan latihan perpus 2

--- src/Perpus/Buku.java
+++ src/Perpus/Buku.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Perpus;
 
-/**
- *
- * @author macbook
- */
 public class Buku {
     private int idBuku, stok;
     private String judulBuku;
